75f5c7e925da9ddb33b7a1debbaa1ca52857fb0f
Rohan7351 <rohan.rana.7351@gmail.com>
2025-10-19 12:12:58 +0530

    Writing Spring Data JPA entities and repo to interact with DB tables.

---
+++ src/main/java/com/banking/account/entity/Customer.java
@@ -0,0 +1,22 @@
+package com.banking.account.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Customer extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="customer_id")
+    private Long customerId;
+
+    @Column(name="name")
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+
+}
---
+++ src/main/java/com/banking/account/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.banking.account.repository;
+
+import com.banking.account.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Customer, Long> {
+
+}
