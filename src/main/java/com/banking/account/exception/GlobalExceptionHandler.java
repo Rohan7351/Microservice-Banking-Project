d7d29c0e6ecaa8a716f931d5590dcd031931ba6f
Rohan7351 <rohan.rana.7351@gmail.com>
2025-10-28 11:48:55 +0530

    Documentation of REST Api using Springboot openAPI specification

--- src/main/java/com/banking/account/exception/GlobalExceptionHandler.java
+++ src/main/java/com/banking/account/exception/GlobalExceptionHandler.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.security.auth.login.AccountNotFoundException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
